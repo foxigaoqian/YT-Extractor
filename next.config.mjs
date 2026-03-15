31676d8af660b4b86cc4faee05a0d7be0c47bc4b
foxigaoqian <foxigaoqian@gmail.com>
2026-03-15 21:48:10 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // 确保 images 配置正确
   images: {
     unoptimized: true,
   },
+  // 确保 output 配置正确
+  output: 'standalone', // 或者不设置这个
 };
 
 export default nextConfig;
